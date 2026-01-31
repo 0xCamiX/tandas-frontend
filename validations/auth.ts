c304196b4e5383414698c174af3334fe1b47e7f8
0xCamix <juancamilogallego70@icloud.com>
2026-01-31 17:41:27 -0500

    feat: Introduce a reusable password validation schema for authentication forms
    - Created a new PasswordSchema to encapsulate password validation rules.
    - Replaced the inline password validation in SignupFormSchema with the new PasswordSchema for improved code maintainability and consistency.

--- validations/auth.ts
+++ validations/auth.ts
@@ -1,5 +1,13 @@
 import * as v from 'valibot'
 
+const PasswordSchema = v.pipe(
+  v.string('Your password must be a string.'),
+  v.nonEmpty('Please enter your password.'),
+  v.minLength(8, 'Your password must have 8 characters or more.'),
+  v.regex(/[0-9]/, 'Your password must include at least one number.'),
+  v.regex(/[A-Za-z]/, 'Your password must include at least one letter.'),
+)
+
 export const SigninFormSchema = v.object({
   email: v.pipe(
     v.string('Your email must be a string.'),
@@ -19,11 +27,7 @@ export const SignupFormSchema = v.object({
     v.nonEmpty('Please enter your email.'),
     v.email('The email address is badly formatted.'),
   ),
-  password: v.pipe(
-    v.string('Your password must be a string.'),
-    v.nonEmpty('Please enter your password.'),
-    v.minLength(8, 'Your password must have 8 characters or more.'),
-  ),
+  password: PasswordSchema,
 })
 
 export type SigninFormValues = v.InferOutput<typeof SigninFormSchema>
